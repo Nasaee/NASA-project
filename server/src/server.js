1cda4c29188b3c0dde27deaa840df26f3cf01b16
Nasa-ee Madadam <122335609+Nasaee@users.noreply.github.com>
2024-01-29 01:20:45 +0700

    fix bug testing with jest.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,28 +1,17 @@
 const http = require('http');
-const mongoose = require('mongoose');
 
 const app = require('./app');
+
+const { mongoConnect } = require('../services/mongo');
 
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
-const MONGO_URL =
-  'mongodb+srv://nasa-api:PPY4AD2pvwty5Mu3@nasacluster.dbhasiv.mongodb.net/?retryWrites=true&w=majority';
-
 const server = http.createServer(app);
 
-// .once() allow to callback only once at the first time it's executed.
-mongoose.connection.once('open', () => {
-  console.log('MongoDB connection ready!');
-});
-
-mongoose.connection.on('error', (err) => {
-  console.error(err);
-});
-
 async function startServer() {
-  await mongoose.connect(MONGO_URL);
+  await mongoConnect();
   await loadPlanetsData(); // load planets data async before starting server
 
   server.listen(PORT, () => {
