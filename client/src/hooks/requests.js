812914fc98ca315cb1ec914d8b59e5c5769df77f
nasaee_window <nasaee.dev@gmail.com>
2024-02-09 01:09:41 +0700

    l

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,13 +1,16 @@
 /*
 remove url berefore v1 because client will know that it's should make request to the api that's living on the same address at the same origin
  */
+
 const API_URL = 'v1';
 
+// Load planets and return as JSON.
 async function httpGetPlanets() {
   const response = await fetch(`${API_URL}/planets`);
   return await response.json();
 }
 
+// Load launches, sort by flight number, and return as JSON.
 async function httpGetLaunches() {
   const response = await fetch(`${API_URL}/launches`);
   const fetchedLaunches = await response.json();
@@ -16,29 +19,31 @@ async function httpGetLaunches() {
   });
 }
 
+// Submit given launch data to launch system.
 async function httpSubmitLaunch(launch) {
   try {
     return await fetch(`${API_URL}/launches`, {
-      method: 'POST',
+      method: 'post',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(launch),
     });
-  } catch (error) {
+  } catch (err) {
     return {
       ok: false,
     };
   }
 }
 
+// Delete launch with given ID.
 async function httpAbortLaunch(id) {
   try {
     return await fetch(`${API_URL}/launches/${id}`, {
       method: 'delete',
     });
-  } catch (error) {
-    console.log(error);
+  } catch (err) {
+    console.log(err);
     return {
       ok: false,
     };
